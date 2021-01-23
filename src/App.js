fd93e6213a3ec8143e16e3d9e9d2fd2d93806fb9
Shahzadrori <shahzadrori@gmail.com>
2021-01-23 12:24:01 +0500

    styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Container from './components/container'
-
+import './App.css';
 function App() {
   return (
     <>
--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 function Container() {
+    // const [city, setcity] = useState(null);
+    // const [search, setsearch] = useState('Fasialabad')
     return (
         <>
         <div className='wraper'>
@@ -9,10 +11,14 @@ function Container() {
             </div>
             <div className='dis'>
                 <h1>dfg</h1>
+                <i className="fas fa-street-view"></i>
             </div>
             <div className='showtem'>
-              <h3>dsfg</h3>
+              <h3>20*</h3>
             </div>
+            <div className='one'></div>
+            <div className='two'></div>
+            <div className='three'></div>
         </div>
         </>
     )
