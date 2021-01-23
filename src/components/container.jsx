511ed643a40ac69bf1a3fa6866bf2e1b8f4ba6a6
Shahzadrori <shahzadrori@gmail.com>
2021-01-23 11:53:43 +0500

    init commit

---
+++ src/components/container.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Container() {
+    return (
+        <>
+        <div className='wraper'>
+            <div className='inp'>
+                <input type='search'></input>
+            </div>
+            <div className='dis'>
+                <h1>dfg</h1>
+            </div>
+            <div className='showtem'>
+              <h3>dsfg</h3>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Container;
