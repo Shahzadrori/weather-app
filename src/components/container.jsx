226f742cb135ac3986f90c423d07f5a9e2ac16e2
Shahzadrori <shahzadrori@gmail.com>
2021-01-25 11:11:05 +0500

    updating

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -2,13 +2,13 @@ import React,{useState,useEffect} from 'react'
 
 function Container() {
     const [city, setcity] = useState(null);
-    const [search, setsearch] = useState('Pune')
+    const [search, setsearch] = useState('Karachi')
     useEffect(() => {
         const fetchapi= async ()=>{
-     const url= `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=b84f47b108fd54f27461f6ec07f83c60`;
+     const url= `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=b84f47b108fd54f27461f6ec07f83c60`;
      const response = await fetch(url);
      const resjson = await response.json();
-      setcity(resjson)
+      setcity(resjson.main)
         }
         fetchapi();
     }, [search])
@@ -16,17 +16,26 @@ function Container() {
         <>
         <div className='wraper'>
             <div className='inp'>
-                <input type='search' onChange={(eve)=> setsearch(eve.target.value)}/>
+            <h1 className='top'>Todays Weather</h1>
+                <input type='search' onChange={(eve)=> setsearch(eve.target.value)} value={search}/>
             </div>
-            <div className='dis'>
+            {!city ? (
+                <p className='nodata'>No Data Found</p>
+            ) : (
+                <>
+                <div className='dis'>
                 <i className="fas fa-street-view stree" ></i>
                 <h2 className='city'>{search}</h2>
             </div>
             <div className='showtem'>
-            <h2>30</h2>
-              <h3>min max</h3>
+            <h2>{city.temp}°C</h2>
+              <h3>Min{city.temp_min}°C || Max{city.temp_max}°C</h3>
             </div>
-        </div>
+        </>
+            )
+
+            }
+            </div>
         </>
     )
 }
