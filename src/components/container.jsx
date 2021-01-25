0380f86aadf1b98f62a24dd01461e40812716c2b
Shahzadrori <shahzadrori@gmail.com>
2021-01-25 10:46:18 +0500

    using useeffect hook to show fetched data

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 function Container() {
-    // const [city, setcity] = useState(null);
-    // const [search, setsearch] = useState('Fasialabad')
+    const [city, setcity] = useState(null);
+    const [search, setsearch] = useState('Pune')
+    useEffect(() => {
+        const fetchapi= async ()=>{
+     const url= `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=b84f47b108fd54f27461f6ec07f83c60`;
+     const response = await fetch(url);
+     const resjson = await response.json();
+      setcity(resjson)
+        }
+        fetchapi();
+    }, [search])
     return (
         <>
         <div className='wraper'>
             <div className='inp'>
-                <input type='search'></input>
+                <input type='search' onChange={(eve)=> setsearch(eve.target.value)}/>
             </div>
             <div className='dis'>
-                <h1>dfg</h1>
-                <i className="fas fa-street-view"></i>
+                <i className="fas fa-street-view stree" ></i>
+                <h2 className='city'>{search}</h2>
             </div>
             <div className='showtem'>
-              <h3>20*</h3>
+            <h2>30</h2>
+              <h3>min max</h3>
             </div>
-            <div className='one'></div>
-            <div className='two'></div>
-            <div className='three'></div>
         </div>
         </>
     )
